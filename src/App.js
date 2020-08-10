346896e8356d0b380b0a87e097582021eb072d86
carcarvroom <carly_l@outlook.com>
2020-08-10 10:54:43 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      toys: data
+    })
   }
 
   handleClick = () => {
@@ -21,20 +28,50 @@ class App extends React.Component{
     })
   }
 
+  createToy = (e) => {
+    e.preventDefault()
+    let newId = this.state.toys[this.state.toys.length -1].id + 1
+    let newToy = {
+      id: newId,
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    let addToy = [...this.state.toys, newToy]
+    this.setState({
+      toys: addToy
+    })
+    this.handleClick()
+  }
+
+  increaseLikes = (toy) => {
+    let updateToy = this.state.toys.map(toys => {
+      if(toys.id === toy.id) {
+        toys.likes += 1
+      } 
+    })
+    this.setState({updateToy})
+  }
+
+  donateGoodwill = (toy) => {
+    let donateToy = this.state.toys.filter(toys => toys.id !== toy.id)
+    this.setState({toys: donateToy})
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm createToy={this.createToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} increaseLikes={this.increaseLikes} donateGoodwill={this.donateGoodwill}/>
       </>
     );
   }
